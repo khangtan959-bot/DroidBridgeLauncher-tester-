5772f80bd23ab39ab88c44dfdb04cf0ec22e426b
Khangtan959 <khangtan959@gmail.com>
2026-05-13 15:45:10 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.ksp.plugin) apply false
-    kotlin("plugin.serialization") version libs.versions.kotlin apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.hilt) apply false
-}
-
 buildscript {
     repositories {
         google()
@@ -18,4 +8,14 @@ buildscript {
         classpath(libs.stringfog.gradle.plugin)
         classpath(libs.stringfog.xor)
     }
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ksp.plugin) apply false
+    kotlin("plugin.serialization") version libs.versions.kotlin apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.hilt) apply false
 }
